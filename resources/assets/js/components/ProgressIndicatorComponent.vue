6e6f24f933b07655328f878bd3affb3a886d6087
Danil <kostynd1@gmail.com>
2021-11-03 00:52:23 +0200

    Create progress indicator

---
+++ resources/assets/js/components/ProgressIndicatorComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="progress-indicator">
+    <div class="progress-indicator__items">
+      <div
+          v-for="(label, index) in labels"
+          class="progress-indicator__item"
+          :class="{'active': index <= getActiveIndex, 'next': index === getActiveIndex + 1}"
+          @click="$emit('click', index + 1, labels[index])"
+      >
+        <span class="progress-indicator__number">{{ index + 1 }}</span>
+        <span class="progress-indicator__label">{{ label }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    labels: {
+      type: Array,
+      required: true
+    },
+    activeIndex: {
+      type: Number,
+      default: 1,
+      required: false
+    }
+  },
+  computed: {
+    getActiveIndex() {
+      return this.activeIndex - 1;
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "../../scss/components/progressIndicator";
+</style>
