4bd8f81469b0a35ce018e1af44d7a8e218db1c96
Danil <kostynd1@gmail.com>
2021-11-02 12:18:29 +0200

    Создан компонент открытого мобильного плеера

--- resources/assets/js/components/Mobile/OpenedMobilePlayerComponent.vue
+++ resources/assets/js/components/Mobile/OpenedMobilePlayerComponent.vue
@@ -1,9 +1,64 @@
 <template>
-
+  <div class="opened-mobile-player">
+    <div class="window_indicator">
+      <div class="window_indicator__border"></div>
+    </div>
+    <div class="opened-mobile-player__image">
+      <img :src="image" :alt="name">
+    </div>
+    <div class="opened-mobile-player__info">
+      <span class="music__name">{{ name }}</span>
+      <span class="music__author">{{ author }}</span>
+    </div>
+    <div class="opened-mobile-player__times">
+      <span class="duration">0:58</span>
+      <span class="total">03:58</span>
+    </div>
+    <progress-bar/>
+    <div class="opened-mobile-player__control">
+      <div class="repeat">
+        <inline-svg src="public/images/icons/repeat.svg"/>
+      </div>
+      <div class="opened-mobile-player__control--center">
+        <div class="prev">
+          <inline-svg src="public/images/icons/prev.svg"/>
+        </div>
+        <div class="play-pause">
+          <inline-svg src="public/images/icons/pause.svg"/>
+        </div>
+        <div class="next">
+          <inline-svg src="public/images/icons/next.svg"/>
+        </div>
+      </div>
+      <div class="shuffle">
+        <inline-svg src="public/images/icons/shuffle.svg"/>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {}
+import ProgressBar from "../ProgressBarComponent";
+
+export default {
+  components: {
+    ProgressBar
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
-
+@import "../../../scss/components/openedMobilePlayer";
 </style>
