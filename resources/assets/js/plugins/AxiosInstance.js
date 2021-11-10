981002b17de8a90b3ac64bad146887a3c3e11af8
Danil <kostynd1@gmail.com>
2021-11-10 02:13:41 +0200

    Обвновлены плагины в соответсвии eslint

--- resources/assets/js/plugins/AxiosInstance.js
+++ resources/assets/js/plugins/AxiosInstance.js
@@ -1,16 +1,18 @@
-import axios from "axios"
-import Cookie from "vue-cookies"
+import axios from "axios";
+import Cookie from "vue-cookies";
 import ConfigModule from "../modules/Config";
 
 export default {
-    install(Vue) {
-        Vue.use(Cookie);
+  install(Vue) {
+    Vue.use(Cookie);
 
-        Vue.prototype.$axios = axios.create({
-            baseURL: ConfigModule.api_url,
-            headers: {
-                Authorization: `Bearer ${Vue.$cookies.get(ConfigModule.auth_cookie_name)}`
-            }
-        });
-    }
-}
+    Vue.prototype.$axios = axios.create({
+      baseURL: ConfigModule.api_url,
+      headers: {
+        Authorization: `Bearer ${Vue.$cookies.get(
+          ConfigModule.auth_cookie_name
+        )}`,
+      },
+    });
+  },
+};
