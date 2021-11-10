e7a4e8aa555918d0a4325c0f527fea9ebdc3c448
Danil <kostynd1@gmail.com>
2021-11-10 02:11:56 +0200

    Обновлена конфигурация в соответствии eslint

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // ENV vars
-const APP_MODE = process.env.APP_MODE;
+const { APP_MODE } = process.env;
 
 // Basic packages
-const mix = require('laravel-mix');
-const path = require('path');
+const mix = require("laravel-mix");
+const path = require("path");
 const fs = require("fs");
-const yaml = require('yaml');
-const notifier = require('node-notifier');
+const yaml = require("yaml");
+const notifier = require("node-notifier");
 
 mix.disableNotifications();
 
 // Packages
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
-const {VueLoaderPlugin} = require('vue-loader');
+const { VueLoaderPlugin } = require("vue-loader");
 
 /**
  * Get config data
@@ -27,35 +27,38 @@ const {VueLoaderPlugin} = require('vue-loader');
  * @returns {any}
  */
 function getConfig(config) {
-    const pathToConfigs = path.resolve(__dirname, 'configs');
-    const configToString = fs.readFileSync(pathToConfigs + config, 'utf8');
+  const pathToConfigs = path.resolve(__dirname, "configs");
+  const configToString = fs.readFileSync(pathToConfigs + config, "utf8");
 
-    return yaml.parse(configToString);
+  return yaml.parse(configToString);
 }
 
 // configs/packages/asset.yaml -> config
-const assetConfig = getConfig('/packages/asset.yaml').asset;
+const assetConfig = getConfig("/packages/asset.yaml").asset;
 
 /**
  * @returns {*}
  * @param type
  */
 function getEntryFiles(type) {
-    return assetConfig.webpack.entryFiles[type];
+  return assetConfig.webpack.entryFiles[type];
 }
 
 /**
  * @returns {{}}
  */
 function getWebpackAliases() {
-    const webpackAliasesFromConfig = assetConfig.webpack.moduleAliases;
-    let webpackAliases = {};
+  const webpackAliasesFromConfig = assetConfig.webpack.moduleAliases;
+  const webpackAliases = {};
 
-    for (let key in webpackAliasesFromConfig) {
-        webpackAliases['@' + key] = path.resolve(__dirname, webpackAliasesFromConfig[key]);
-    }
+  for (const key in webpackAliasesFromConfig) {
+    webpackAliases[`@${key}`] = path.resolve(
+      __dirname,
+      webpackAliasesFromConfig[key]
+    );
+  }
 
-    return webpackAliases;
+  return webpackAliases;
 }
 
 const assetsPath = assetConfig.paths.assets;
@@ -63,90 +66,74 @@ const distPath = assetConfig.paths.dist;
 
 // Build
 mix
-    .js(
-        assetsPath + getEntryFiles('js'),
-        distPath + 'js'
-    )
-    .sass(
-        assetsPath + getEntryFiles('sass'),
-        distPath + 'css'
-    )
-    .options({
-        processCssUrls: false,
-        postCss: [
-            require('postcss-preset-env'),
-            require('autoprefixer')({
-                cascade: false
-            }),
-            require('postcss-custom-properties'),
-            require('postcss-sort-media-queries'),
-        ]
-    })
-    .vue()
-    .webpackConfig({
-        mode: APP_MODE,
-        resolve: {
-            extensions: [
-                '.js',
-                '.json',
-                '.vue'
-            ],
-            alias: getWebpackAliases()
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: distPath + 'css/app.css'
-            }),
-            new CopyPlugin({
-                patterns: [
-                    {from: assetsPath + 'images', to: distPath + 'images'}
-                ]
-            }),
-            new ImageminWebpWebpackPlugin({
-                config: [{
-                    test: /\.(jpe?g|png)/,
-                    options: {
-                        quality: 75
-                    }
-                }]
-            }),
-            new CopyPlugin({
-                patterns: [
-                    {from: assetsPath + 'fonts', to: distPath + 'fonts'}
-                ]
-            }),
-            new VueLoaderPlugin()
+  .js(assetsPath + getEntryFiles("js"), `${distPath}js`)
+  .sass(assetsPath + getEntryFiles("sass"), `${distPath}css`)
+  .options({
+    processCssUrls: false,
+    postCss: [
+      require("postcss-preset-env"),
+      require("autoprefixer")({
+        cascade: false,
+      }),
+      require("postcss-custom-properties"),
+      require("postcss-sort-media-queries"),
+    ],
+  })
+  .vue()
+  .webpackConfig({
+    mode: APP_MODE,
+    resolve: {
+      extensions: [".js", ".json", ".vue"],
+      alias: getWebpackAliases(),
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `${distPath}css/app.css`,
+      }),
+      new CopyPlugin({
+        patterns: [{ from: `${assetsPath}images`, to: `${distPath}images` }],
+      }),
+      new ImageminWebpWebpackPlugin({
+        config: [
+          {
+            test: /\.(jpe?g|png)/,
+            options: {
+              quality: 75,
+            },
+          },
         ],
-        optimization: {
-            minimize: APP_MODE !== 'development',
-            minimizer: [
-                new TerserPlugin({
-                    test: /\.js$/,
-                    extractComments: assetConfig.webpack.extractJsComments
-                }),
-                new CssMinimizerPlugin({
-                    test: /.(css|scss|sass)/,
-                    exclude: /(node_modules|bower_components)/,
-                })
-            ]
-        }
-    }).then(function (stats) {
-        const jsonStats = stats.toJson();
-        let message = 'Successful build';
+      }),
+      new VueLoaderPlugin(),
+    ],
+    optimization: {
+      minimize: APP_MODE !== "development",
+      minimizer: [
+        new TerserPlugin({
+          test: /\.js$/,
+          extractComments: assetConfig.webpack.extractJsComments,
+        }),
+        new CssMinimizerPlugin({
+          test: /.(css|scss|sass)/,
+          exclude: /(node_modules|bower_components)/,
+        }),
+      ],
+    },
+  })
+  .then((stats) => {
+    const jsonStats = stats.toJson();
+    let message = "Successful build";
 
-        if(jsonStats.errors.length > 0) {
-            message = 'Error: ' + jsonStats.errors[0].message;
-        } else if(jsonStats.warnings.length > 0) {
-            message = 'Warning: ' + jsonStats.warnings[0].message;
-        }
+    if (jsonStats.errors.length > 0) {
+      message = `Error: ${jsonStats.errors[0].message}`;
+    } else if (jsonStats.warnings.length > 0) {
+      message = `Warning: ${jsonStats.warnings[0].message}`;
+    }
 
-        notifier.notify(
-            {
-                title: 'Codememory',
-                message: message,
-                icon: path.join(__dirname, 'public/assets/images/codememory_icon.png'),
-                sound: true,
-                wait: true
-            }
-        );
+    notifier.notify({
+      title: "Codememory",
+      message,
+      icon: path.join(__dirname, "public/assets/images/codememory_icon.png"),
+      sound: true,
+      wait: true,
     });
+  });
