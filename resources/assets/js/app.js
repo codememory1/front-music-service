a7cd5325d4946cc283db54a13380efb49bb66aab
Danil <kostynd1@gmail.com>
2021-11-10 02:12:55 +0200

    Обвновлен app.js в соответсвии eslint

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import router from './routes/router'
+import Vue from "vue";
+import TouchEvents from "vue2-touch-events";
+import InlineSvg from "vue-inline-svg";
+import router from "./routes/router";
 
 // Import plugins
-import AxiosInstance from './plugins/AxiosInstance'
-import TouchEvents from 'vue2-touch-events'
+import AxiosInstance from "./plugins/AxiosInstance";
 
 // Import components
-import InlineSvg from 'vue-inline-svg'
 
 Vue.use(AxiosInstance);
 Vue.use(TouchEvents);
 
-Vue.component('inline-svg', InlineSvg);
+Vue.component("InlineSvg", InlineSvg);
 
-Vue.productionTip = false
+Vue.productionTip = false;
 
 new Vue({
-    el: '#app',
-    router
-})
+  el: "#app",
+  router,
+});
