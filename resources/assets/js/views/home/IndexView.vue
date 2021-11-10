4f01f82accafb3735ff36b60cf3e888a5f11712a
Danil <kostynd1@gmail.com>
2021-11-10 02:15:19 +0200

    Обвновлен indexView в соответствии eslint

--- resources/assets/js/views/home/IndexView.vue
+++ resources/assets/js/views/home/IndexView.vue
@@ -1,65 +1,7 @@
 <template>
   <div class="content">
-    <!-- Start header -->
-    <header class="header">
-      <div class="container">
-        <div class="header__navbar">
-          <div class="header__logo">
-            <img src="public/images/logo.png" alt="logo">
-          </div>
-          <div class="navbar__right">
-            <div class="header__navigation">
-              <ul class="navigation__items">
-                <li class="navigation__item">
-                  <a href="#">Premium</a>
-                </li>
-                <li class="navigation__item">
-                  <a href="#">Help</a>
-                </li>
-                <li class="navigation__item">
-                  <a href="#">Download</a>
-                </li>
-                <li class="navigation__item">
-                  <a href="#">Sign up</a>
-                </li>
-                <li class="navigation__item">
-                  <a href="#">Log in</a>
-                </li>
-              </ul>
-            </div>
-            <div class="header__select--lang">
-              <custom-select
-                  :class="'header__lang'"
-                  :placeholder="'Выберите язык'"
-                  :options="[
-                      {
-                        label: 'English',
-                        value: 'en'
-                      },
-                      {
-                        label: 'Russian',
-                        value: 'ru'
-                      }
-                  ]"
-			  />
-            </div>
-          </div>
-        </div>
-        <div class="header__content">
-          <div class="header__content--left">
-            <h1>Listening is everything in your life</h1>
-            <p>
-              Millions of songs and podcasts. No credit card. of songs and podcasts. No credit card.
-            </p>
-            <a href="#" class="header__content--button">Create account</a>
-          </div>
-          <div class="header__content--right">
-            <img src="public/images/mockup.png" alt="mockup">
-          </div>
-        </div>
-      </div>
-    </header>
-    <!-- End header -->
+    <!--  Header component -->
+    <the-home-header />
 
     <div class="container">
       <!-- Start sections  advantages -->
@@ -68,69 +10,77 @@
         <div class="advantages">
           <div class="advantage">
             <div class="advantage__icon">
-              <inline-svg src="public/images/icons/unique-features.svg"/>
+              <inline-svg src="public/images/icons/unique-features.svg" />
             </div>
             <div class="advantage__info">
               <span class="advantage__title">Unique features</span>
               <p class="advantage__text">
-                Since your most unique feature is your brand's voice, build out your content using your blog.
+                Since your most unique feature is your brand's voice, build out
+                your content using your blog.
               </p>
             </div>
           </div>
           <div class="advantage">
             <div class="advantage__icon">
-              <inline-svg src="public/images/icons/subscription-price.svg"/>
+              <inline-svg src="public/images/icons/subscription-price.svg" />
             </div>
             <div class="advantage__info">
               <span class="advantage__title">Subscription price</span>
               <p class="advantage__text">
-                Like a rights issue or open offer, it allows shareholders to buy additional shares, usually at a fixed
-                price.
+                Like a rights issue or open offer, it allows shareholders to buy
+                additional shares, usually at a fixed price.
               </p>
             </div>
           </div>
           <div class="advantage">
             <div class="advantage__icon">
-              <inline-svg src="public/images/icons/more-options.svg"/>
+              <inline-svg src="public/images/icons/more-options.svg" />
             </div>
             <div class="advantage__info">
-              <span class="advantage__title">More options for free subscription</span>
+              <span class="advantage__title"
+                >More options for free subscription</span
+              >
               <p class="advantage__text">
-                A freemium app allows customers to use the app at no cost, with the option to subscribe.
+                A freemium app allows customers to use the app at no cost, with
+                the option to subscribe.
               </p>
             </div>
           </div>
           <div class="advantage">
             <div class="advantage__icon">
-              <inline-svg src="public/images/icons/unique-features.svg"/>
+              <inline-svg src="public/images/icons/unique-features.svg" />
             </div>
             <div class="advantage__info">
               <span class="advantage__title">Unique features</span>
               <p class="advantage__text">
-                Since your most unique feature is your brand's voice, build out your content using your blog.
+                Since your most unique feature is your brand's voice, build out
+                your content using your blog.
               </p>
             </div>
           </div>
           <div class="advantage">
             <div class="advantage__icon">
-              <inline-svg src="public/images/icons/subscription-price.svg"/>
+              <inline-svg src="public/images/icons/subscription-price.svg" />
             </div>
             <div class="advantage__info">
               <span class="advantage__title">Subscription price</span>
               <p class="advantage__text">
-                Like a rights issue or open offer, it allows shareholders to buy additional shares, usually at a fixed
-                price.
+                Like a rights issue or open offer, it allows shareholders to buy
+                additional shares, usually at a fixed price.
               </p>
             </div>
           </div>
           <div class="advantage">
             <div class="advantage__icon">
-              <inline-svg src="public/images/icons/more-options.svg"/>
+              <inline-svg src="public/images/icons/more-options.svg" />
             </div>
             <div class="advantage__info">
-              <span class="advantage__title">More options for free subscription</span>
+              <span class="advantage__title"
+                >More options for free subscription</span
+              >
               <p class="advantage__text">
-                A freemium app allows customers to use the app at no cost, with the option to subscribe.
+                A freemium app allows customers to use the app at no cost, with
+                the option to subscribe.
               </p>
             </div>
           </div>
@@ -141,43 +91,63 @@
       <!-- Start sections subscriptions -->
       <section class="subscriptions">
         <h2 class="subscriptions__title">Choose your subscription</h2>
-        <p class="subscriptions__text">Listen without limits on your phone, speaker and other devices</p>
+        <p class="subscriptions__text">
+          Listen without limits on your phone, speaker and other devices
+        </p>
 
         <div class="subscription__cards">
-          <subscription
-              :id="1"
-              :title="'Basic'"
-              :mini-desc="'Essential feature'"
-              :old-price="59.99"
-              :price="53.99"
-              :is-valid="5"
-              :options="['Access to all features', '5 Watchlists included', '10+ exclusive premium widgets', 'Priority support', 'Another great feature']"
+          <base-subscription
+            :id="1"
+            :title="'Basic'"
+            :mini-desc="'Essential feature'"
+            :old-price="59.99"
+            :price="53.99"
+            :is-valid="5"
+            :options="[
+              'Access to all features',
+              '5 Watchlists included',
+              '10+ exclusive premium widgets',
+              'Priority support',
+              'Another great feature',
+            ]"
           />
-          <subscription
-              :id="2"
-              :title="'Pro'"
-              :mini-desc="'Advanced feature'"
-              :old-price="89.99"
-              :price="80.99"
-              :is-valid="5"
-              :options="['Access to all features', '5 Watchlists included', '10+ exclusive premium widgets', 'Priority support', 'Another great feature']"
-              :background="'#3C56D6'"
+          <base-subscription
+            :id="2"
+            :title="'Pro'"
+            :mini-desc="'Advanced feature'"
+            :old-price="89.99"
+            :price="80.99"
+            :is-valid="5"
+            :options="[
+              'Access to all features',
+              '5 Watchlists included',
+              '10+ exclusive premium widgets',
+              'Priority support',
+              'Another great feature',
+            ]"
+            :background="'#3C56D6'"
           >
             <template v-slot:bang>
               <subscription-bang
-                  :title="'Рекомендуют'"
-                  :background="'#3C56D6'"
+                :title="'Рекомендуют'"
+                :background="'#3C56D6'"
               />
             </template>
-          </subscription>
-          <subscription
-              :id="1"
-              :title="'Basic'"
-              :mini-desc="'Essential feature'"
-              :old-price="59.99"
-              :price="53.99"
-              :is-valid="5"
-              :options="['Access to all features', '5 Watchlists included', '10+ exclusive premium widgets', 'Priority support', 'Another great feature']"
+          </base-subscription>
+          <base-subscription
+            :id="1"
+            :title="'Basic'"
+            :mini-desc="'Essential feature'"
+            :old-price="59.99"
+            :price="53.99"
+            :is-valid="5"
+            :options="[
+              'Access to all features',
+              '5 Watchlists included',
+              '10+ exclusive premium widgets',
+              'Priority support',
+              'Another great feature',
+            ]"
           />
         </div>
       </section>
@@ -185,24 +155,24 @@
     </div>
 
     <!-- Footer component -->
-    <custom-footer/>
+    <the-footer />
   </div>
 </template>
 <script>
-import Subscription from "../../components/SubscriptionComponent"
-import SubscriptionBang from "../../components/SubscriptionBangComponent";
-import CustomFooter from "../../components/CustomFooterComponent";
-import CustomSelect from "../../components/SelectComponent"
+import TheHomeHeader from "../../components/Headers/TheHomeHeaderComponent";
+import BaseSubscription from "../../components/Subscription/BaseSubscriptionComponent";
+import SubscriptionBang from "../../components/Subscription/SubscriptionBangComponent";
+import TheFooter from "../../components/TheFooterComponent";
 
 export default {
   components: {
-    Subscription,
+    TheHomeHeader,
+    BaseSubscription,
     SubscriptionBang,
-    CustomFooter,
-    CustomSelect
-  }
-}
+    TheFooter,
+  },
+};
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 @import "../../../scss/views/home";
 </style>
