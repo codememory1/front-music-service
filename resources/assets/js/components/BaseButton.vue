70bf46b91c295b2f85af400b79ff4ab0e0132421
Danil <kostynd1@gmail.com>
2021-11-12 01:40:59 +0200

    Базовая кнопка

---
+++ resources/assets/js/components/BaseButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="button" @click="$emit('click')">{{ value }}</button>
+</template>
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss">
+@import "../../scss/variables";
+
+.button {
+  font-size: 16px;
+  font-weight: 500;
+  background-color: $accent;
+  padding: 14px 30px;
+  border-radius: 6px;
+  color: #fff;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+
+  &:hover {
+	background-color: darken($accent, 10%);
+  }
+}
+</style>
