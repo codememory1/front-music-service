c6ff0c7376f53b35f24fca198e623804735e01b4
Danil <kostynd1@gmail.com>
2021-11-16 17:26:37 +0200

    Добавлен маршрут artist-view

--- resources/assets/js/routes/player.js
+++ resources/assets/js/routes/player.js
@@ -1,1 +1,16 @@
-export const routes = [];
+import PlayerLayout from "../layouts/PlayerLayout";
+import ArtistView from "../views/player/ArtistView";
+
+export const routes = [
+  {
+    path: "*",
+    component: PlayerLayout,
+    name: "playerLayout",
+    children: [
+      {
+        path: ":id",
+        component: ArtistView
+      }
+    ]
+  }
+];
