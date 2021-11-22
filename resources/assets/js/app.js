9253a39977821838a77703fcda3d33bcbe7d9d0a
Danil <kostynd1@gmail.com>
2021-11-22 15:48:16 +0200

    Use vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,20 @@
 import Vue from "vue";
 import router from "./routes/router";
+import Store from "./store";
+
+// Import Components
 import TouchEvents from "vue2-touch-events";
 import InlineSvg from "vue-inline-svg";
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 import ImageAlias from "./components/Globals/ImageAliasComponent";
 import InlineSvgAlias from "./components/Globals/InlineSvgAliasComponent";
 
-// Import plugins
+// Import Plugins
 import AxiosInstance from "./plugins/AxiosInstance";
 import IsEmpty from "./plugins/IsEmpty";
 import Storage from "./plugins/Storage";
+
+// Import Directives
 import VTooltip from "v-tooltip";
 
 Vue.use(AxiosInstance);
@@ -17,6 +22,7 @@ Vue.use(TouchEvents);
 Vue.use(IsEmpty);
 Vue.use(Storage);
 Vue.use(VTooltip);
+Vue.use(Store);
 
 Vue.component("InlineSvg", InlineSvg);
 Vue.component("CoreSwiper", Swiper);
